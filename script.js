e49a6d87d1f8db1fe0ddc800bb9690c58da4390d
André Rodrigues <andre360.ars@gmail.com>
2021-12-26 21:09:17 -0300

    Requisito 4 - Cria funções que geram codigos rgb aleatórios

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let colors = document.getElementById('colors');
+let balls = document.getElementsByClassName('ball')
+let colorRgb = []
 
 
+
+// Requisito 3
 function createCircle(num) {
     for (let i = 0; i < num; i += 1) {
         let circle = document.createElement('div');
@@ -11,3 +15,28 @@ function createCircle(num) {
 }
 
 createCircle(6)
+
+
+// Requisto 4
+// feito a partir do codigo do requisito 12 do
+// projeto pixels art
+
+function rgbGenerator() {
+    let rgb = []
+    
+    for (let i = 0; i < 3; i += 1) {
+        rgb.push(Math.floor(Math.random() * 255) + 1)
+    }
+
+    return '(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')' 
+}
+
+function randomColors() {
+    for (let index = 0; index < balls.length; index += 1){
+        let color = rgbGenerator();
+
+        colorRgb.push(color)
+    }
+}
+
+randomColors()
